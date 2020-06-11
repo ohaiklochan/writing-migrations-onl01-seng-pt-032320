3fd95ca5cce999341802f6e43daf8093de751a85
Chloe Steffens <caforonda103@yahoo.com>
2020-06-11 04:53:44 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,8 +1,8 @@
-# class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   
-#   def change
-#     change_column :table_name, :column_name, :type
-#   end
+  def change
+    change_column :table_name, :column_name, :type
+  end
   
 
-# end
+end
