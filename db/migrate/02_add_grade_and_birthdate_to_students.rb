857182db8873928c5eb150f8d890cf76b60dd0f3
Chloe Steffens <caforonda103@yahoo.com>
2020-06-11 04:46:23 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,7 +1,7 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   
   def change
-    add_column :grade, :birthdate
+    add_column :students, :grade, :birthdate, :string
   end
   
 
