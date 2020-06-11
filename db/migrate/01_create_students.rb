1734a02e46643154950bf9fdd98c5304b7ddca90
Chloe Steffens <caforonda103@yahoo.com>
2020-06-11 04:11:03 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -3,8 +3,6 @@ class CreateStudents < ActiveRecord::Migration[5.2]
   def change
     create_table :students do |t|
       t.string :name
-      t.integer :grade
-      t.string :birthdate
     end
   end
   
